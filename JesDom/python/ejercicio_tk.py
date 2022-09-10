71ea65361d348c7d2a58bd2f9c9f81917479cb3a
Lotito30 <112903235+Lotito30@users.noreply.github.com>
2022-09-10 10:41:38 -0300

    Add files via upload

---
+++ JesDom/python/ejercicio_tk.py
@@ -0,0 +1,12 @@
+class Calculadora:
+    def __init__(self, valor1, valor2):
+        self.valor1 = valor1
+        self.valor2 = valor2
+    def Suma(self,valor1,valor2):
+        
+    
+    def Multiplicacion(self,valor1,valor2):
+
+calculadora = Tk()
+calculadora.Calculadora
+calculadora.main.loop
